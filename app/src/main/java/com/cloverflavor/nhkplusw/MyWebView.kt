aa18073ad24b454fcb67e66003f07ee93bc34bc5
SASAKI Hideo <sasaki@rio.ne.jp>
2021-09-20 19:30:08 +0900

    changed remote control behavior

--- app/src/main/java/com/cloverflavor/nhkplusw/MyWebView.kt
+++ app/src/main/java/com/cloverflavor/nhkplusw/MyWebView.kt
@@ -14,10 +14,64 @@ class MyWebView(context: Context, attributeSet: AttributeSet) : WebView(context,
         var handled = false
         when (keyCode) {
             KeyEvent.KEYCODE_MENU -> {
+                evaluateJavascript("""
+                    var slideShows = document.getElementsByTagName('main')[0].ariaHidden;
+                    if (slideShows == null || slideShows == 'false') {
+                        document.getElementById('searchbtn').click();
+                        document.getElementById('keyword').focus();
+                    } else if (document.querySelectorAll('#tab-calender.-active').length > 0) { 
+                        document.getElementById('searchbtn').click();
+                        document.querySelectorAll('#swiper-wrapper a')[0].focus();
+                    } else {
+                        document.getElementById('tab-calender').click();
+                        document.getElementById('channel_G').focus();
+                    }
+                    """
+                ) { }
                 handled = true
-                this.evaluateJavascript("document.getElementById('searchbtn').click()", null)
-                this.evaluateJavascript("document.getElementById('keyword').focus()", null)
             }
+//            KeyEvent.KEYCODE_MEDIA_REWIND -> {
+//                evaluateJavascript("""
+//                    if (document.getElementsByTagName('main')[0].ariaHidden == 'false') {
+//                        document.querySelectorAll('#swiper-wrapper a')[0].focus();
+//                    } else {
+//                        document.getElementById('tab-keyword').click();
+//                        document.getElementById('keyword').click();
+//                    }
+//                    """
+//                ) { }
+//                handled = true
+//            }
+//            KeyEvent.KEYCODE_MEDIA_FAST_FORWARD -> {
+//                evaluateJavascript("""
+//                    if (document.getElementsByTagName('main')[0].ariaHidden == 'false') {
+//                        document.querySelectorAll('#swiper-wrapper a')[1].focus();
+//                    } else {
+//                        document.getElementById('tab-calender').click();
+//                        document.getElementById('channel_G').focus();
+//                    }
+//                    """
+//                ) { }
+//                handled = true
+//            }
+//            KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE -> {
+//                evaluateJavascript("""
+//                    document.activeElement.click();
+//                    """
+//                ) { }
+//                handled = true
+//            }
+//            KeyEvent.KEYCODE_BACK -> {
+//                evaluateJavascript("""
+//                    if (document.getElementsByTagName('main')[0].ariaHidden == 'false') {
+//                        return 'false';
+//                    } else {
+//                        document.getElementById('searchbtn').click();
+//                        return 'true';
+//                    }
+//                    """
+//                ) { handled = it.toBoolean() }
+//            }
         }
         return handled || super.onKeyDown(keyCode, event)
     }
